9a2be20b704d6dd3dc2d21ce8ada5e729ac10d26
黄逸晖 <huangyihui@ihyht.com>
2018-08-07 18:04:32 +0800

    整合接口打通下单流程(Feign)

--- order/src/main/java/com/example/order/controller/OrderController.java
+++ order/src/main/java/com/example/order/controller/OrderController.java
@@ -1,6 +1,10 @@
 package com.example.order.controller;
 
+import com.example.order.client.ProductClient;
+import com.example.order.dataObject.OrderDetail;
 import com.example.order.dataObject.OrderMaster;
+import com.example.order.dataObject.ProductInfo;
+import com.example.order.dto.CartDTO;
 import com.example.order.dto.OrderDTO;
 import com.example.order.enums.OrderStatusEnum;
 import com.example.order.enums.PayStatusEnum;
@@ -10,25 +14,31 @@ import com.example.order.utils.keyUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by 黄逸晖 on 2018-08-06.
  */
+@SuppressWarnings("SpringJavaAutowiringInspection")
 @RestController
 @RequestMapping("/order")
 @Slf4j
 public class OrderController {
-
-    @SuppressWarnings("SpringJavaAutowiringInspection")
     @Autowired
     private OrderMasterRepository orderMasterRepository;
-    @SuppressWarnings("SpringJavaAutowiringInspection")
+
     @Autowired
     private OrderDetailRepository orderDetailRepository;
+
+    @Autowired
+    private ProductClient productClient;
     /**
      * 1. 参数检验
      * 2. 查询商品信息(调用商品服务)
@@ -37,16 +47,45 @@ public class OrderController {
      * 5. 订单入库
      */
 
+    @PostMapping("/create")
     public OrderDTO create(OrderDTO orderDTO){
+        String orderId = keyUtil.genUniqueKey();
+        //参数检验
+        //查询商品信息(调用商品服务)
+        List<String> productIdList = orderDTO.getOrderDetailList().stream()
+                .map(OrderDetail::getProductId)
+                .collect(Collectors.toList());
+        List<ProductInfo> productInfoList = productClient.listForOrder(productIdList);
+        //计算总价
+        BigDecimal orderAmount = BigDecimal.ZERO;
+        for(OrderDetail orderDetail : orderDTO.getOrderDetailList()){
+            for (ProductInfo productInfo : productInfoList){
+                if(productInfo.getProductId().equals(orderDetail.getProductId())){
+                    //单价*数量
+                    orderAmount = productInfo.getProductPrice()
+                            .multiply(new BigDecimal(orderDetail.getProductQuantity()))
+                            .add(orderAmount);
+                    BeanUtils.copyProperties(productInfo,orderDetail);
+                    orderDetail.setOrderId(orderId);
+                    orderDetail.setDetailId(keyUtil.genUniqueKey());
+                    orderDetailRepository.save(orderDetail);
+                }
+            }
+        }
+        //扣库存(调用商品服务)
+        List<CartDTO> cartDTOList = orderDTO.getOrderDetailList().stream()
+                .map(e -> new CartDTO(e.getProductId(),e.getProductQuantity()))
+                .collect(Collectors.toList());
+        productClient.decreaseProductStock(cartDTOList);
+
+        //订单入库
         OrderMaster orderMaster = new OrderMaster();
-        orderDTO.setOrderId(keyUtil.genUniqueKey());
+        orderDTO.setOrderId(orderId);
         BeanUtils.copyProperties(orderDTO,orderMaster);
-        orderMaster.setOrderAmount(new BigDecimal(5));
+        orderMaster.setOrderAmount(orderAmount);
         orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
         orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
-
         orderMasterRepository.save(orderMaster);
-
         return orderDTO;
 
     }
